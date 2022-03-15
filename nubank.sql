1297f324c3928980f00b879b3c858f83245850a8
Prsferreira8 <prsferreira4@gmail.com>
2022-03-14 21:27:20 -0400

    Banco de Dados - Nubank
    Criação de banco de dados no mysql

---
+++ nubank.sql
@@ -0,0 +1,108 @@
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: nubank
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `funcionarios`
+--
+
+DROP TABLE IF EXISTS `funcionarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `funcionarios` (
+  `id` int(11) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `telefone` varchar(10) DEFAULT NULL,
+  `cargo` varchar(10) DEFAULT NULL,
+  `salario` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcionarios`
+--
+
+LOCK TABLES `funcionarios` WRITE;
+/*!40000 ALTER TABLE `funcionarios` DISABLE KEYS */;
+INSERT INTO `funcionarios` VALUES (5,'Jiraia da Silva Souza','12312312332','8745212320','Caixa','3.500,00');
+/*!40000 ALTER TABLE `funcionarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pessoa_fisica`
+--
+
+DROP TABLE IF EXISTS `pessoa_fisica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pessoa_fisica` (
+  `id` int(11) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `ag` varchar(10) DEFAULT NULL,
+  `conta` varchar(10) DEFAULT NULL,
+  `pix` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa_fisica`
+--
+
+LOCK TABLES `pessoa_fisica` WRITE;
+/*!40000 ALTER TABLE `pessoa_fisica` DISABLE KEYS */;
+INSERT INTO `pessoa_fisica` VALUES (1,'Pedro Alvares Cabral','12312312332','123','123','pedro@portugal.com'),(2,'Sasuke Uchiha da Silva','1593576859','132','231','sasuketransao@gmail.com'),(3,'Naruto Uzumaki Bezerra','8524713694','245','102','narutodafolha@outlook.com'),(4,'Batman de Souza Araújo','1234567899','258','789','maislokoqobatman@live.com'),(5,'Ronaldo Ronaldim Rubens','9632587412','652','584','ronaldim@golzao.com');
+/*!40000 ALTER TABLE `pessoa_fisica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pessoa_juridica`
+--
+
+DROP TABLE IF EXISTS `pessoa_juridica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pessoa_juridica` (
+  `id` int(11) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cnpj` varchar(14) DEFAULT NULL,
+  `ag` varchar(10) DEFAULT NULL,
+  `conta` varchar(10) DEFAULT NULL,
+  `pix` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa_juridica`
+--
+
+LOCK TABLES `pessoa_juridica` WRITE;
+/*!40000 ALTER TABLE `pessoa_juridica` DISABLE KEYS */;
+INSERT INTO `pessoa_juridica` VALUES (1,'Padim Padi Çiçu','12312312332584','133','432','pedim@cicu.com'),(2,'Çergil Malandro da Silva','12345678998765','123','123','bilu@teteia.com'),(3,'Bartolomeu Simpsom de Souza','01233214566547','144','186','bart@simpson.com'),(4,'Homer Simpsom Costa','78945632145698','985','584','simpson@homer.com'),(5,'Jiraia da Silva Souza','12312312332','451','235','umadadivadosninjas@nj.com');
+/*!40000 ALTER TABLE `pessoa_juridica` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-14 21:19:40
