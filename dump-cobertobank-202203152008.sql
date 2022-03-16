01ca147d388a4286449251d9c39e0d594812819e
Prsferreira8 <prsferreira4@gmail.com>
2022-03-15 20:09:14 -0400

    cobertorbank
    Novo banco de dados criado a partir da interface gráfica do DBiver

---
+++ dump-cobertobank-202203152008.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: cobertobank
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pessoa_fisica`
+--
+
+DROP TABLE IF EXISTS `pessoa_fisica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pessoa_fisica` (
+  `id` int(100) DEFAULT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `ag` varchar(10) DEFAULT NULL,
+  `conta` varchar(10) DEFAULT NULL,
+  `pix` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa_fisica`
+--
+
+LOCK TABLES `pessoa_fisica` WRITE;
+/*!40000 ALTER TABLE `pessoa_fisica` DISABLE KEYS */;
+INSERT INTO `pessoa_fisica` VALUES (1,'Naruto Uzumaki','01236532061','68912334566','0210','2013654','naruto@folha.com'),(2,'Sasuke Uchiha','45678112354','97841125633','8574','1032478','sasuketransao@folha.com'),(3,'Batman de Souza','45874445899','78945512361','7485','1254789','+lokoqobatman@gotam.com');
+/*!40000 ALTER TABLE `pessoa_fisica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'cobertobank'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-15 20:08:15
